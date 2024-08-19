5b6b078f7e5c8e9ee356bb80e24dfb8f9d4897b7
Stephen <scpwalker@hotmail.com>
2024-08-19 09:55:23 +0100

    modified code for boxes

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,7 +1,8 @@
-// external js: draggabilly.pkgd.js
-//touching file to push
-
-let draggie = new Draggabilly(".draggable");
-draggie.on("staticClick", function () {
-  console.log("staticClick");
-});
+// Initialize Draggabilly for all draggable elements
+document
+  .querySelectorAll(
+    ".draggable1, .draggable2, .draggable3, .draggable4, .draggable5, .draggable6, .draggable7, .draggable8"
+  )
+  .forEach((item) => {
+    new Draggabilly(item);
+  });
